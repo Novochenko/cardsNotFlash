864509e897fe4ee08a56586dc6b2a84d8ec180f0
gartermion <159763003+gartermion@users.noreply.github.com>
2024-05-08 11:48:51 +0300

    adada

--- auth/auth_reg.js
+++ auth/auth_reg.js
@@ -1,10 +1,7 @@
 // Получаем элементы формы
 const form = document.getElementById('register-form');
-//const nicknameInput = document.getElementById("nickname");
-//const emailInput = document.getElementById("email");
-//const passwordInput = document.getElementById("password");
 const registerButton = document.getElementById('register-btn');
-const responseDiv = document.getElementById('register-response');
+//const data = document.getElementById('register-response');
 
 
 // Добавляем обработчик события на кнопку регистрации
@@ -29,26 +26,18 @@ registerButton.addEventListener('click', (e) => {
     },
     body: JSON.stringify(userData)
   })
-  .then((responseDiv) => responseDiv.json())
+  .then((response) => response.text())
   .then((data) => {
+    document.getElementById('register-response').innerHTML = data;
     if (data.success) {
-      responseDiv.innerHTML = 'Пользователь зарегистрирован успешно!';
+      data.innerHTML = 'Пользователь зарегистрирован успешно!';
       window.location.href = "../main/main.html"
-    } else {
-      responseDiv.innerHTML = 'Ошибка регистрации: ' + data.error;
+    } 
+    else {
+      data.innerHTML = 'Ошибка регистрации: ' + data.error;
     }
   })
-  .catch((error) => {
+  .catch(error => {
     console.error(error);
   });
 });
-
-
-/*<span>Или используйте e-mail для регистрации</span>
-<label for="nickname">Никнейм</label>
-<input type="text" id="nickname" >
-<label for="email">E-mail</label>
-<input id="email" type="email" >
-<label for="password">Пароль</label>
-<input id="password" type="password">
-<button id="register-btn">Создать</button>*/
