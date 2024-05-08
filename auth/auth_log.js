6879701347038493d5be38399c86af4bb133536b
gartermion <159763003+gartermion@users.noreply.github.com>
2024-05-08 16:19:01 +0300

    rabota

--- auth/auth_log.js
+++ auth/auth_log.js
@@ -6,8 +6,8 @@ const responseLogin = document.getElementById('login-response');
   // Добавляем обработчик события на кнопку регистрации
   loginButton.addEventListener('click', (e) => {
     e.preventDefault();
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    const email = document.getElementById('email-log').value;
+    const password = document.getElementById('password-log').value;
   
     const userData = {
       email,
@@ -15,21 +15,26 @@ const responseLogin = document.getElementById('login-response');
     };
   
     // Отправляем запрос на сервер
-    fetch("http://127.0.0.1:9000/sessions", {
+    fetch("http://26.229.38.10:9000/sessions", {
       method: 'POST',
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify(userData)
     })
-    .then((response) => response.json())
-    .then(data => {
-      if (data.success) {
-        responseLogin.innerHTML = 'Пользователь вошел успешно!';
-        window.location.href = "../main/main.html"
-      } else {
-        responseLogin.innerHTML = 'Ошибка регистрации: ' + data.error;
+    .then((response) =>
+    {
+      if (response.ok){
+        console.log('ok')
+        window.location.href= "../main/main.html"
       }
+      else{
+        console.log('error')
+      }
+      return response.json()
+    })
+    .then((data) => {
+      console.log(data)
     })
     .catch(error => {
       console.error(error);
