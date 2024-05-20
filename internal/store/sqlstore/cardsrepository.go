8ead51e9e19261c7af282aff1c87e72c66a76e9b
Novochenko <pukichegor@mail.ru>
2024-05-20 16:55:41 +0300

    usingtime fix2

--- internal/store/sqlstore/cardsrepository.go
+++ internal/store/sqlstore/cardsrepository.go
@@ -69,7 +69,7 @@ func (cr *CardRepository) Delete(c *model.Card) error {
 
 func (cr *CardRepository) Show(c *model.Card) ([]*model.Card, error) {
 	cards := []*model.Card{}
-	rows, err := cr.store.db.Query("SELECT card_id, user_id, front_side, back_side, card_time, time_flag FROM cards WHERE user_id = ? AND group_id = ?;", c.UserID, c.GroupID)
+	rows, err := cr.store.db.Query("SELECT card_id, user_id, front_side, back_side, card_time, time_flag FROM cards WHERE user_id = ?;", c.UserID)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return cards, store.ErrRecordNotFound
@@ -117,8 +117,8 @@ func (cr *CardRepository) ShowUsingTime(c *model.Card) ([]*model.Card, error) {
 	cards := []*model.Card{}
 	rows, err := cr.store.db.Query(`SELECT card_id, front_side, back_side, time_flag, card_time, time_flag
 									FROM cards
-								 	WHERE user_id = ? AND TIMEDIFF(NOW(), card_time) > time_flag;
-								 `, c.UserID)
+								 	WHERE user_id = ? AND group_id = ? AND TIMEDIFF(NOW(), card_time) > time_flag;
+								 `, c.UserID, c.GroupID)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return cards, store.ErrRecordNotFound
