8b91f75775a502326e035d61d3baa2a243976040
gartermion <159763003+gartermion@users.noreply.github.com>
2024-06-01 19:51:41 +0300

    ura

--- del/delete.js
+++ del/delete.js
@@ -1,6 +1,3 @@
-let cardList = []
-
-
 fetch('https://localhost:443/private/show',{
     method: 'GET',
     credentials: 'include',
@@ -19,7 +16,7 @@ return response.json()})
     
 .then(data => {
       // Вставляем опции в select
-      const cardList = document.getElementById("delete-card")
+      const cardList = document.getElementById("delete-card"); 
       data.forEach(card => {
         const cardElement = document.createElement('div');
         cardElement.innerHTML = `
@@ -29,42 +26,40 @@ return response.json()})
           <div class="card-back"><strong>${card.back_side}</strong></div>
         </div>
         `;
+        cardElement.dataset.id = card.id;
         cardList.appendChild(cardElement);
-        })
+      });
+      const listItems = cardList.children;
+      for (let i = 0; i < listItems.length; i++) {
+        listItems[i].addEventListener('click', function() {
+          // Выбрали элемент, теперь можно взаимодействовать с ним
+          const selectedOption = this.dataset.id;
+          console.log(`Выбран элемент №: ${selectedOption}`);
+        
+          if(selectedOption){
+            console.log('rrerere');
+            const id = selectedOption;
+            const card_id = parseInt(id);
+            const cardDel = {
+              "card_id": card_id
+            }
+            fetch('https://localhost:443/private/deletecard',{
+              method: 'POST',
+              credentials: 'include',
+              headers:{
+                "Content-Type": "application/json"
+              },
+              body:JSON.stringify(cardDel)
+            })
+            .then(response =>{
+              if(response.ok){
+                console.log('delete success')
+              }
+              else{
+                console.log('delete error')
+              }
+            })
+          }
 })
-.catch(error => {
-  `Ошибка отправки вопроса: ${error}`;
-});
-function deleteCard() {
-  const deleteButton = document.getElementById('del-btn');
-  const selectedOption = document.getElementById('container-list').selectedOptions[0];
-
-  if(selectedOption){
-    console.log('rrerere');
-    deleteButton.addEventListener('click', (e) => {
-      e.preventDefault();
-      const id = selectedOption.value;
-      const card_id = parseInt(id);
-      const cardDel = {
-        "card_id": card_id
-      }
-      fetch('https://localhost:443/private/deletecard',{
-        method: 'POST',
-        credentials: 'include',
-        headers:{
-          "Content-Type": "application/json"
-        },
-        body:JSON.stringify(cardDel)
-      })
-      .then(response =>{
-        if(response.ok){
-          console.log('delete success')
-        }
-        else{
-          console.log('delete error')
-        }
-      })
-    })
-  }
 }
-  
+})
