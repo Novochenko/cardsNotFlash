54a1806840e0d618b4171d2a1a170a53e7b53bdc
Novochenko <pukichegor@mail.ru>
2024-06-06 22:26:12 +0300

    meow

--- user/user.js
+++ user/user.js
@@ -41,7 +41,7 @@ fetch('https://localhost:443/private/lkshow/', {
       userLk.innerHTML=`
       <div class="wrapper">
           <div class="left">
-              <img id="uploaded-image" src="file:///C:/Users/Alex/Desktop/cardsNotFlesh/images/pfpimages/1.png" alt="Uploaded Image" width="100">
+              <img id="uploaded-image" src="" alt="Uploaded Image" width="100">
               <h4>${nickname}</h4>
                <p>Designer</p>
                <input type="file" id="file-input" accept="image/png"/>
